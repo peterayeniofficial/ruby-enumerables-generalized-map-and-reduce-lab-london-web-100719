bc260d0788ace27c4b123f6214315acf17ddcb54
Peter Ayeni <ayenicology@gmail.com>
2019-09-07 11:33:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(source_array)
+  yield(source_array)
+end
+
+map([1,2,3,4]) {|n| n}
+map([1,2,3,4]) {|n| n * -1}
+
+
+def reduce(source_array, starting = 0 ) 
+  yield(source_array )
+end
+
+reduce([1,2,3]){|memo, n| memo + n}
